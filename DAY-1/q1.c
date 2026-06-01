e88abaac3407e1601f070baef6cf04c39fc8eb0f
devchaudhary704-dot <devchaudhary704@gmail.com>
2026-06-01 15:32:11 +0530

    question 1 and 2 solved

---
+++ DAY-1/q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, sum = 0;
+    printf("enter the number: ");
+    scanf("%d", &n);
+    for (i = 0; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    printf("sum of n number is %d", sum);
+    return 0;
+}
