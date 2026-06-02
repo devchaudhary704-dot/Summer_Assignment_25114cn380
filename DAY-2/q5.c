d95b7ee91919f51848ee6270dd1330944a8fc6f2
devchaudhary704-dot <devchaudhary704@gmail.com>
2026-06-02 20:20:07 +0530

    question 5 solved

---
+++ DAY-2/q5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int n,d,sum=0;
+    printf("enter the number :");
+    scanf("%d",&n);
+    while(n>0){
+        d=n%10;
+        sum=sum+d;
+        n=n/10;}
+        printf("the sum of digit is %d",sum);
+    
+    return 0;
+}
